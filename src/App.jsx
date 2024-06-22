69687662d5d52337f0437e2a8ee05586b632d5f8
Elias <manchai.fung@gmail.com>
2024-06-22 11:31:25 -0400

    added font and theme

--- src/App.jsx
+++ src/App.jsx
@@ -10,33 +10,51 @@ import LoginForm from './pages/LoginForm/LoginForm';
 import './App.css';
 import RegisterPage from './pages/RegisterPage/RegisterPage';
 import { Route, Routes } from 'react-router-dom';
-import { Box } from '@mui/material';
+import { Box, ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#F2565B'
+    },
+    secondary: {
+      main: '#2F6A9F'
+    }
+  },
+  typography: {
+    fontFamily: [
+      'Kulim Park'
+    ]
+  }
+})
 
 const App = () => {
   const [section, setSection] = useState('home');
 
   return (
-    <Box>
-      <Navbar setSection={setSection} />
-      {section === 'home' && (
-        <>
-          <Section1 />
-          <Section2 />
-        </>
-      )}
-      {section === 'menu' && <Menu />}
-      {section === 'nosotros' && <Nosotros />}
-      {section === 'contacto' && <Contacto />}
-      {section === 'iniciarSesion' && <LoginForm />}
-      {section === 'registrarse' && <Registrarse />}
-      
-      <Routes>
-        <>
-        <Route path="/login" element={<LoginForm />}/>
-        <Route path="/register" element={<RegisterPage />}/>
-        </>
-      </Routes>
-    </Box>
+    <ThemeProvider theme={theme}>
+      <Box>
+        <Navbar setSection={setSection} />
+        {section === 'home' && (
+          <>
+            <Section1 />
+            <Section2 />
+          </>
+        )}
+        {section === 'menu' && <Menu />}
+        {section === 'nosotros' && <Nosotros />}
+        {section === 'contacto' && <Contacto />}
+        {section === 'iniciarSesion' && <LoginForm />}
+        {section === 'registrarse' && <Registrarse />}
+        
+        <Routes>
+          <>
+          <Route path="/login" element={<LoginForm />}/>
+          <Route path="/register" element={<RegisterPage />}/>
+          </>
+        </Routes>
+      </Box>
+    </ThemeProvider>
   );
 };
 
