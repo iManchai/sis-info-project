bb8c55238eff0cc90d734f7245c535be52d29584
Elias <manchai.fung@gmail.com>
2024-06-28 19:36:42 -0400

    make basics functions for plates

---
+++ src/controllers/plates.js
@@ -0,0 +1,52 @@
+import { addDoc, collection, getDoc, getDocs, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+
+export async function getPlates() {
+  const plateCollections = collection(db, 'plates');
+
+  const plateDocs = await getDocs(plateCollections);
+  const plates = plateDocs.docs.map(doc => doc.data())
+
+  return plates
+}
+
+export async function getPlate(id) {
+  const plateCollections = collection(db, 'plates');
+
+  const plateDocs = await getDoc(plateCollections, id);
+  const plate = plateDocs.data()
+
+  return plate
+}
+
+export async function createPlate({title, type, description, price, image} ) {
+  const plateCollections = collection(db, 'plates');
+
+  const plateDoc = await addDoc(plateCollections, {
+    title,
+    type,
+    description,
+    price,
+    image
+  });
+  
+  const id = plateDoc.id
+
+  return id
+}
+
+export async function updatePlate(id, { title, type, description, price, image} ) {
+  const plateCollections = collection(db, 'plates');
+
+  await updateDoc(doc(plateCollections, id), {
+    title,
+    type,
+    description,
+    price,
+    image
+  });
+}
+
+export async function deletePlate() {
+
+}
